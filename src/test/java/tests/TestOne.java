b0614424050c6089a2484745f43eeca163e3d257
vanessastre <vanessastre@gmail.com>
2023-05-03 11:42:09 -0300

    Update TestOne.java

--- src/test/java/tests/TestOne.java
+++ src/test/java/tests/TestOne.java
@@ -4,6 +4,7 @@ import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 import pages.HomePage;
+import pages.LoginPage;
 
 public class TestOne extends TestBase {
 
@@ -15,5 +16,9 @@ public class TestOne extends TestBase {
 	public void testTC01() {
 		HomePage homePage = new HomePage(driver);
 		homePage.checkHomePage();
+		homePage.clickSignIn();
+
+		LoginPage loginPage = new LoginPage(driver);
+		loginPage.inputName("roni_cost@example.com");
 	}
 }
